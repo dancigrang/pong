1b0f1a33eedb448ad3c2e59827495275e5b7cc96
Daniel Cigrang <danielcigrang@me.com>
2013-07-30 23:53:52 -0500

    Created a ball
    ball moves with arrow keys

--- pong.py
+++ pong.py
@@ -1,4 +1,25 @@
-import pygame
+import pygame, os, sys
+from pygame.locals import *
+
+class Ball():
+	def __init__(self):
+		self.rect = pygame.Rect(295,200,10,10)
+		# pygame.draw.rect(background, (250,250,250), (295,200,10,10), 0)
+
+	def move(self, dx, dy):
+		# print 'update'
+		self.rect.x += dx
+		self.rect.y += dy
+
+class Paddle():
+	def __init__(self):
+		self.rect = pygame.Rect(295,200,100,10)
+		# pygame.draw.rect(background, (250,250,250), (295,200,10,10), 0)
+
+	def move(self, dx, dy):
+		# print 'update'
+		self.rect.x += dx
+		self.rect.y += dy
 
 def main():
 	pygame.init()
@@ -9,7 +30,31 @@ def main():
 	background = background.convert()
 	background.fill((0, 0, 0))
 
-	screen.blit(background, (0,0))
-	pygame.display.flip()
+	# screen.blit(background, (0,0))
+	# pygame.display.flip()
+
+	b = Ball()
+	clock = pygame.time.Clock()
+
+	while True:
+		clock.tick(60)
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				return False
+			if event.type == KEYDOWN and event.key == K_ESCAPE:
+				return False
+			if event.type == KEYDOWN and event.key == K_UP:
+				b.move(0,-10)
+			if event.type == KEYDOWN and event.key == K_DOWN:
+				b.move(0,10)
+			if event.type == KEYDOWN and event.key == K_LEFT:
+				b.move(-10,0)
+			if event.type == KEYDOWN and event.key == K_RIGHT:
+				b.move(10,0)
+
+		screen.blit(background, (0,0))
+		background.fill((0, 0, 0))
+		pygame.draw.rect(background, (250,250,250), b.rect, 0)
+		pygame.display.flip()
 	
 main()
