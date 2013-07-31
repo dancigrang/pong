742a5f1e9239253730041fe0847cd3a9218a6475
pvacey <pfvacey@ilstu.edu>
2013-07-30 22:16:07 -0500

    added window

--- pong.py
+++ pong.py
@@ -1,3 +1,16 @@
 import pygame
 
-print 'hello dan'
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode((600,400))
+	pygame.display.set_caption('Pong')
+
+	background = pygame.Surface(screen.get_size())
+	background = background.convert()
+	background.fill((0, 0, 0))
+
+	screen.blit(background, (0,0))
+	pygame.display.flip()
+
+	
+main()
